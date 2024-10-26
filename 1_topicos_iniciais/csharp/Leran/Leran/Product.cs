54da0ca9bcb3b2eeda45c48091cb1f728bf428dd
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2024-10-26 12:46:10 -0300

    csharp

---
+++ 1_topicos_iniciais/csharp/Leran/Leran/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leran
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) { 
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name +" | " + " R$" + Price + " | " + Quantity;
+        }
+
+        public double total()
+        {
+            return Price * Quantity;
+        }
+        public void updatePrice(double percentage)
+        {
+            Price = Price * (1.0 + percentage / 100.0);
+        }
+    }
+}
---
+++ 1_topicos_iniciais/csharp/Leran/Leran/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Leran // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        //sem OO
+        static double total(Product product)
+        {
+            return product.Price * product.Quantity;
+        }
+        static void updatePrice(Product product, double percentage)
+        {
+            product.Price = product.Price * (1.0 + percentage / 100.0);
+        }
+        static void Main(string[] args)
+
+        {
+            var p1 = new Product("Laptop", 1000.0, 5);
+            var p2 = new Product("Headphone", 200.0, 2);
+
+            var total1 = total(p1);
+            var total2 = p2.total;
+
+            updatePrice(p1, 10.0);
+            p2.updatePrice(50);
+
+            Console.WriteLine(total1);
+            Console.WriteLine(p2.Price);
+
+            Console.WriteLine(p1.Price);
+
+        }
+    }
+}
